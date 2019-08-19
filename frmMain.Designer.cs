fc392fbc3e8c0e3b0650919fabb23f3654fdca4b
nithinreddy98 <44168290+nithinreddy98@users.noreply.github.com>
2019-08-19 20:18:47 +0530

    Add files via upload

---
+++ frmMain.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace WavStagno
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtFilePath = new System.Windows.Forms.TextBox();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnHide = new System.Windows.Forms.Button();
+            this.btnExtract = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.dlgBrowseFile = new System.Windows.Forms.OpenFileDialog();
+            this.dlgSaveFile = new System.Windows.Forms.SaveFileDialog();
+            this.lblMessageLength = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtMessage = new System.Windows.Forms.RichTextBox();
+            this.comWorker = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(48, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "File Path";
+            // 
+            // txtFilePath
+            // 
+            this.txtFilePath.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFilePath.Location = new System.Drawing.Point(119, 37);
+            this.txtFilePath.Name = "txtFilePath";
+            this.txtFilePath.Size = new System.Drawing.Size(230, 25);
+            this.txtFilePath.TabIndex = 1;
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBrowse.Location = new System.Drawing.Point(355, 37);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(75, 25);
+            this.btnBrowse.TabIndex = 1;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(48, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Message";
+            // 
+            // btnHide
+            // 
+            this.btnHide.Enabled = false;
+            this.btnHide.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnHide.Location = new System.Drawing.Point(355, 226);
+            this.btnHide.Name = "btnHide";
+            this.btnHide.Size = new System.Drawing.Size(75, 25);
+            this.btnHide.TabIndex = 4;
+            this.btnHide.Text = "Hide";
+            this.btnHide.UseVisualStyleBackColor = true;
+            this.btnHide.Click += new System.EventHandler(this.btnHide_Click);
+            // 
+            // btnExtract
+            // 
+            this.btnExtract.Enabled = false;
+            this.btnExtract.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExtract.Location = new System.Drawing.Point(274, 226);
+            this.btnExtract.Name = "btnExtract";
+            this.btnExtract.Size = new System.Drawing.Size(75, 25);
+            this.btnExtract.TabIndex = 3;
+            this.btnExtract.Text = "Extract";
+            this.btnExtract.UseVisualStyleBackColor = true;
+            this.btnExtract.Click += new System.EventHandler(this.btnExtract_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(119, 226);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 25);
+            this.btnClear.TabIndex = 5;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // dlgBrowseFile
+            // 
+            this.dlgBrowseFile.FileName = "*.wav";
+            this.dlgBrowseFile.Filter = "Wave Audio | *.wav";
+            this.dlgBrowseFile.Title = "Choose File to Hide Message";
+            this.dlgBrowseFile.FileOk += new System.ComponentModel.CancelEventHandler(this.dlgBrowseFile_FileOk);
+            // 
+            // dlgSaveFile
+            // 
+            this.dlgSaveFile.DefaultExt = "*.wav";
+            this.dlgSaveFile.FileName = "File1";
+            this.dlgSaveFile.Filter = "Wave Audio | *.wav";
+            this.dlgSaveFile.Title = "Save File";
+            this.dlgSaveFile.FileOk += new System.ComponentModel.CancelEventHandler(this.dlgSaveFile_FileOk);
+            // 
+            // lblMessageLength
+            // 
+            this.lblMessageLength.AutoSize = true;
+            this.lblMessageLength.Location = new System.Drawing.Point(212, 200);
+            this.lblMessageLength.Name = "lblMessageLength";
+            this.lblMessageLength.Size = new System.Drawing.Size(13, 13);
+            this.lblMessageLength.TabIndex = 8;
+            this.lblMessageLength.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(116, 200);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(90, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "No. of Characters";
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtMessage.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMessage.Location = new System.Drawing.Point(119, 81);
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(311, 117);
+            this.txtMessage.TabIndex = 2;
+            this.txtMessage.Text = "";
+            this.txtMessage.TextChanged += new System.EventHandler(this.txtMessage_TextChanged);
+            // 
+            // comWorker
+            // 
+            this.comWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.comWorker_DoWork);
+            this.comWorker.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.comWorker_RunWorkerCompleted);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(480, 276);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblMessageLength);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnExtract);
+            this.Controls.Add(this.btnHide);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.txtFilePath);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "frmMain";
+            this.Text = "WavStagno 1.0";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtFilePath;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnHide;
+        private System.Windows.Forms.Button btnExtract;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.OpenFileDialog dlgBrowseFile;
+        private System.Windows.Forms.SaveFileDialog dlgSaveFile;
+        private System.Windows.Forms.Label lblMessageLength;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RichTextBox txtMessage;
+        private System.ComponentModel.BackgroundWorker comWorker;
+    }
+}
+
---
+++ frmMain.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+using WavStagno.Media;
+
+namespace WavStagno
+{
+    public partial class frmMain : Form
+    {
+        private WaveAudio file;
+        private StagnoHelper sh;
+        private string message;
+        private bool HIDE_ERROR;
+
+        public frmMain()
+        {
+            InitializeComponent();
+            HIDE_ERROR = false;
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            dlgBrowseFile.ShowDialog();
+        }
+
+        private void dlgBrowseFile_FileOk(object sender, CancelEventArgs e)
+        {
+            txtFilePath.Text = dlgBrowseFile.FileName;
+            if (txtFilePath.Text.Trim() != "")
+            {
+                btnExtract.Enabled = true;
+                btnHide.Enabled = true;
+                file = new WaveAudio(new FileStream(txtFilePath.Text, FileMode.Open, FileAccess.Read));
+                sh = new StagnoHelper(file);
+            }
+        }
+
+        private void btnExtract_Click(object sender, EventArgs e)
+        {
+            message = "";
+            message = sh.ExtractMessage();
+            txtMessage.Text = message;
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtMessage.Text = "";
+        }
+
+        private void btnHide_Click(object sender, EventArgs e)
+        {
+            message = txtMessage.Text.Trim();
+            if (message == "")
+                MessageBox.Show(this, "Write Message to Hide!", "WavStagno 1.0", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            else
+            {
+                btnHide.Enabled = false;
+                btnExtract.Enabled = false;
+                this.Cursor = Cursors.WaitCursor;
+                comWorker.RunWorkerAsync();
+            }
+        }
+
+        private void dlgSaveFile_FileOk(object sender, CancelEventArgs e)
+        {
+            file.WriteFile(dlgSaveFile.FileName);
+            file = new WaveAudio(new FileStream(txtFilePath.Text, FileMode.Open, FileAccess.Read));
+            sh = new StagnoHelper(file);
+        }
+
+        private void txtMessage_TextChanged(object sender, EventArgs e)
+        {
+            lblMessageLength.Text = txtMessage.TextLength.ToString();            
+        }
+
+        private void comWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            try
+            {
+                sh.HideMessage(message);
+            }
+            catch (MessageSizeExceededException ex)
+            {
+                HIDE_ERROR = true;
+            }
+        }
+
+        private void comWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (!HIDE_ERROR)
+            {
+                btnHide.Enabled = true;
+                btnExtract.Enabled = true;
+                this.Cursor = Cursors.Default;
+                dlgSaveFile.ShowDialog();
+            }
+            else
+                MessageBox.Show(this, "Message size is too large!", "WavStagno 1.0", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+    }
+}
